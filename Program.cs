d2d0f460afd091aa45823bd252e4ce2078247cb3
Diana <lici4qweru090@gmail.com>
2022-10-29 17:30:00 +0500

    first commit

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace гит
 {
     internal class Program
+    //Z1.10. Найти все двузначные числа, которые делятся на n или содержат цифру n.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("раз два три");
+            Console.WriteLine("введине число деления: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            list
+            for (int i = 10; i < 100; i++)
+            {
+                if (i % n == 0)
+                { 
+                    
+                }
+            }
+
+
         }
     }
 }
