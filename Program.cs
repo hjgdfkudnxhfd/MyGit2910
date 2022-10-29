368a52914134dd5e61168fb939e22bf48f56f221
Diana <lici4qweru090@gmail.com>
2022-10-29 15:12:40 +0500

    inish commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace гит
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("раз два три");
+        }
+    }
+}
